5d48b4ea95a60df4291407f6500fe0809a227a09
17sepdav <david@seppi.org>
2022-02-02 09:17:54 +0100

    MapMarker

--- src/app/services/marker.service.ts
+++ src/app/services/marker.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import * as L from 'leaflet';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +8,11 @@ import { HttpClient } from '@angular/common/http';
 export class MarkerService {
 
   constructor(private http: HttpClient) { }
+
+  makeCapitalMarkers(map: L.Map): void {
+    const lon = 10.9;
+    const lat = 49.8;
+    const marker = L.marker([lat, lon]);
+    marker.addTo(map);
+   }
 }
